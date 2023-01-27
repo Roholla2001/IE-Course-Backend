4aa291af85b0b262f1c34c84231da00ce7e930d1
Roholla Ahmadzadeh <roholla2001@gmail.com>
2023-01-27 04:14:29 +0330

    Update main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,4 +1,15 @@
 package main
 
-func main(){
+import (
+	"log"
+
+	"github.com/Roholla2001/ie-course-backend/cmd/boot"
+)
+
+func main() {
+	// starting our golang web server
+	err := boot.BootServer()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
