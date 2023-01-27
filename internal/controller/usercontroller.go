8ce67e934bd79dc51e229ecc3114fed1736eb23b
Roholla Ahmadzadeh <roholla2001@gmail.com>
2023-01-27 04:46:28 +0330

    add user controllers

--- internal/controller/usercontroller.go
+++ internal/controller/usercontroller.go
@@ -1,6 +1,9 @@
 package controller
 
-import "gorm.io/gorm"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type UserController struct {
 }
@@ -9,3 +12,10 @@ func NewUserController(db *gorm.DB) (*UserController,error){
 	return &UserController{}, nil
 }
 
+func Register(ctx *gin.Context){
+
+}
+
+func Login(ctx *gin.Context){
+	
+}
