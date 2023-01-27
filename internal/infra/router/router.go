f1c394a9677580d7af42a45073c2651e3b6d4b02
Roholla Ahmadzadeh <roholla2001@gmail.com>
2023-01-27 04:45:28 +0330

    Re-structure controllers

--- internal/infra/router/router.go
+++ internal/infra/router/router.go
@@ -1,57 +1,13 @@
 package router
 
 import (
-	"fmt"
-
-	"github.com/Roholla2001/ie-course-backend/internal/adaptor"
+	"github.com/Roholla2001/ie-course-backend/internal/controller"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-type AppController struct {
-	ServerController *adaptor.ServerController
-	UserController   *adaptor.UserController
-	URLController    []*adaptor.URLController
-}
-
-func InitRouter(ac *AppController) *gin.Engine {
+func InitRouter(ac *controller.AppController) *gin.Engine {
 	router := gin.Default()
 	ac.AddRoutes(&router.RouterGroup)
 
 	return router
-}
-
-func (ac *AppController) AddRoutes(parent *gin.RouterGroup) {
-}
-
-func (ac *AppController) NewServerController(db *gorm.DB) (*adaptor.ServerController, error) {
-	return adaptor.NewServerController(db)
-}
-
-func (ac *AppController) NewUserController(db *gorm.DB) (*adaptor.UserController, error) {
-	return adaptor.NewUserController(db)
-}
-
-
-func (ac *AppController) NewURLController(db *gorm.DB) ([]*adaptor.URLController, error) {
-	if ac.ServerController == nil {
-		return nil, fmt.Errorf("Cannot initiate url controller without a server")
-	}
-
-	urls, err := ac.ServerController.GetURLs()
-	if err != nil {
-		return nil, err
-	}
-
-	urlControllers := make([]*adaptor.URLController, 0, len(urls))
-	for _, url := range urls {
-		uc, err := adaptor.NewURLController(db, url)
-		if err != nil {
-			return nil, err
-		}
-
-		urlControllers = append(urlControllers, uc)
-	}
-
-	return urlControllers, nil
 }
